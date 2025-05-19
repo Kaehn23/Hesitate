699f4acf0a92ad0e54fddd744a55ed3ebcd3eb61
Jeremy Ipa <jeremy.iparraguirre@gmail.com>
2025-05-19 11:19:13 +0200

    fix 1

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['fr', 'en'],      // langues supportées
-    defaultLocale: 'fr',        // langue par défaut
-  },
+  
 };
 
 export default nextConfig;
